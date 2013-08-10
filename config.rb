df00602185c8efc2119b13dd77d05a7773a5d6cd
Alex Miles <alexmiles@Alexs-MacBook-Air.local>
2013-08-10 12:34:51 +0200

    Configure project

--- config.rb
+++ config.rb
@@ -36,7 +36,7 @@
 # activate :automatic_image_sizes
 
 # Reload the browser automatically whenever files change
-# activate :livereload
+activate :livereload
 
 # Methods defined in the helpers block are available in templates
 # helpers do
@@ -63,7 +63,8 @@ configure :build do
   # activate :asset_hash
 
   # Use relative URLs
-  # activate :relative_assets
+  activate :relative_assets
+  set :relative_links, true
 
   # Or use a different image path
   # set :http_path, "/Content/images/"
