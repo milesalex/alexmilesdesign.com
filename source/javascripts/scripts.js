16c49d46a6818c0e689be0dcdbd6f0e75b2db880
Alex Miles <howdy@alexmilesdesign.com>
2013-08-17 17:37:35 +0200

    Fix styles and add description

--- source/javascripts/scripts.js
+++ source/javascripts/scripts.js
@@ -1,24 +1,33 @@
-(function($) {
   
 	function init(){
-		var winHeight = $(window).height();
-		$('.repeat').height(winHeight * 2);
+		// var winHeight = $(window).height();
+		// $('.repeat').height(winHeight * 2);
 		
-		var docHeight = $(document).height();
-		var fullScroll = docHeight - winHeight;
-		
-		$(window).scroll(function () {
-	  	var scrollPos = $(window).scrollTop();
+		// var docHeight = $(document).height();
+		// var fullScroll = docHeight - winHeight;
 
-	  	if (scrollPos >= (fullScroll)) {
-	  		$('.work').css('margin-top', (winHeight * 1.5) + 'px');
-	  		$(document).scrollTop(0);
-	  	}
+		$('.test').click(function(){
+			// window.scrollTo(0, 0);
+
+			
+  		
 		});
+		
+	 //  	var scrollPos = $(window).scrollTop();
+
+	 //  	if (scrollPos >= (fullScroll)) {
+	 //  		$('.work').css('margin-top', (winHeight * 1.5) + 'px');
+	 //  		$(document).scrollTop(0);
+	 //  	}
+		// });
 	};
 
 	$(window).bind("load", function() {
+		
+		var top = $('.work').offset().top;
+		console.log(top);
+		$(window).scrollTop(top);
+
 		init();
 	});
 
-})(jQuery);
